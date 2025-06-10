f4a2c8ccba7802a871c4bbd2a9ffde7c58ce604b
Hai Pham <thanh_hai1290@yahoo.com>
2025-06-10 21:19:00 +0700

    fix: change environment variable

--- db/pool.js
+++ db/pool.js
@@ -2,9 +2,9 @@ const { Pool } = require("pg");
 require("dotenv").config();
 
 module.exports = new Pool({
-  host: process.env.HOST,
-  user: process.env.USERNAME,
-  database: process.env.DATABASE,
-  password: process.env.PASSWORD,
-  port: process.env.DB_PORT,
+  host: process.env.PGHOST,
+  user: process.env.PGUSER,
+  database: process.env.PGDATABASE,
+  password: process.env.PGPASSWORD,
+  port: process.env.PGPORT,
 });
